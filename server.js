30b80ea861c824aadcb36b4e9c202e03ae2a4fb2
yungukbae <yungukbae@gmail.com>
2021-02-19 01:51:43 +0900

    server3

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const http = require('http');
 
-http.createServer((req,res) => {
+const server = http.createServer((req,res) => {
     res.write('<h1>Hello</h1>');
     res.end('<p>nodejs</p>');
-}).listen(8080, () => {
+});
+
+server.listen(8080);
+
+server.on('listening',()=>{
     console.log('8080 port is ready...');
 });
+
+server.on('error', (error) => {
+    console.log('server on error');
+});
+
