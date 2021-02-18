fc5fc3bc0962a0c7c6686d49584177f86845b30c
yungukbae <yungukbae@gmail.com>
2021-02-19 01:47:54 +0900

    server2

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require('http');
 
 http.createServer((req,res) => {
-    //어떻게 응답할지 작성
+    res.write('<h1>Hello</h1>');
+    res.end('<p>nodejs</p>');
+}).listen(8080, () => {
+    console.log('8080 port is ready...');
 });
