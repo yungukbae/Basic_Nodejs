94072cd6a960d4e36781642ccdb9a4879058ab42
yungukbae <yungukbae@gmail.com>
2021-02-19 02:39:17 +0900

    server4 cookie

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req,res) => {
-   fs.readFile('./index.html',(err,data)=>{
-       if(err){ //if there's an error.
-           throw err;   //throw error.
-       }
-       res.end(data);   //send data(buffer)
-   });
-}).listen(8081, () => {
-    console.log('8081 port ready...');
+const parseCookies = (cookie = '') => 
+    cookie 
+        .split(';')
+        .map(v => v.split('='))
+        .map(([k, ...vs]) => [k, vs.join('=')])
+        .reduce((acc, [k,v]) => {
+            acc[k.trim()] = decodeURIComponent(v);
+            return acc;
+        },{});
+
+http.createServer((req,res) => {
+    const cookies = parseCookies(req.headers.cookie); //cookie is in req.headers.cookie
+    console.log(req.url, cookies);
+    res.writeHead(200, {'Set-Cookie':'mycookie=test'}); //res.writeHeader = recording cookie
+    res.end('Hello Cookie');
+}).listen(8082, () => {
+    console.log('8082 port is ready...');
 });
