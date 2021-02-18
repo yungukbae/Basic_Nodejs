5549e915369459ed91ed16da3a29b98c46e106da
yungukbae <yungukbae@gmail.com>
2021-02-19 03:05:39 +0900

    server5 cookie

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
 
 const parseCookies = (cookie = '') => 
-    cookie 
+    cookie
         .split(';')
         .map(v => v.split('='))
         .map(([k, ...vs]) => [k, vs.join('=')])
@@ -11,10 +14,28 @@ const parseCookies = (cookie = '') =>
         },{});
 
 http.createServer((req,res) => {
-    const cookies = parseCookies(req.headers.cookie); //cookie is in req.headers.cookie
-    console.log(req.url, cookies);
-    res.writeHead(200, {'Set-Cookie':'mycookie=test'}); //res.writeHeader = recording cookie
-    res.end('Hello Cookie');
-}).listen(8082, () => {
-    console.log('8082 port is ready...');
+    const cookies = parseCookies(req.headers.cookie);
+    if(req.url.startsWith('/login')){   
+        const {query} = url.parse(req.url);
+        const {name} = qs.parse(query);
+        const expires = new Date();
+        expires.setMinutes(expires.getMinutes() + 5); //cookie end time: 5minute
+        res.writeHead(302,{
+            Location: '/',
+            'Set-Cookie':`name=${encodeURIComponent(name)}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`,
+        }); //redirect addres, header get cookie data
+        res.end();
+    }else if(cookies.name){ 
+        res.writeHead(200,{'Content-Type':'text/html; charset=utf-8'});
+        res.end(`Hello ${cookies.name}`);       //view name
+    }else{ 
+        fs.readFile('./index.html',(err,data) => {
+            if(err){
+                throw err;
+            }
+            res.end(data);  //response html
+        });
+    }
+}).listen(8083, () => {
+    console.log('8083 port is ready..');
 });
